8b1859d4a44a81fde177842cf512f253b1345948
Tatjana <malina91983@gmail.com>
2026-05-26 10:26:32 +0200

    Async problem demo

--- orderservice/src/main/java/org/example/orderservice/controller/ChaosScenario.java
+++
@@ -1,9 +0,0 @@
-package org.example.orderservice.controller;
-
-public enum ChaosScenario {
-    NORMAL,
-    FAIL_BEFORE_PUBLISH,
-    DUPLICATE_MESSAGE,
-    DATA_CORRUPTION,
-    TRANSIENT_FAILURE
-}
--- orderservice/src/main/java/org/example/orderservice/repository/OutboxRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.orderservice.repository;
-
-import org.example.orderservice.model.OutboxEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface OutboxRepository extends JpaRepository<OutboxEvent, Long> {
-    List<OutboxEvent> findByStatus(OutboxEvent.OutboxStatus status);
-}
