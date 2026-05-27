a4af3a8c236c44629123a76d78af8955b2ad726d
Tatjana <malina91983@gmail.com>
2026-05-27 19:52:03 +0200

    Updates lab2

--- stockservice/src/main/java/org/example/stockservice/controller/ChaosScenario.java
+++
@@ -1,9 +0,0 @@
-package org.example.stockservice.controller;
-
-public enum ChaosScenario {
-    NORMAL,
-    FAIL_BEFORE_PUBLISH,
-    DUPLICATE_MESSAGE,
-    DATA_CORRUPTION,
-    TRANSIENT_FAILURE
-}
--- stockservice/src/main/java/org/example/stockservice/repository/ProcessedEventRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.stockservice.repository;
-
-import org.example.stockservice.model.ProcessedEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface ProcessedEventRepository extends JpaRepository<ProcessedEvent, UUID> {}
